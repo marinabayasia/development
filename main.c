0bf7119605bfd98378ac10e8d78d64bff6714e9d
MARINA BAY <marinabayasia@gmail.com>
2019-12-30 16:51:13 -0400

    hello

--- main.c
+++ main.c
@@ -1,3 +1,11 @@
-int main (void)
-    {printf ("hello, world\n");
+#include<stdio.h>
+
+int main(void) {
+    int sz = 100;
+    int a[sz];
+
+    for (int i = 0; i < sz; ++i) {
+    a[i] = i * i;
     }
+    printf('bye...\n');
+}
