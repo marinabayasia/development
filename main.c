aa41569c9bbef695670544de3aa279b953d70354
MARINA BAY <marinabayasia@gmail.com>
2019-12-31 18:33:05 -0400

    Here I tried to print the words

--- main.c
+++ main.c
@@ -2,16 +2,18 @@
 #include "util.h"
 
 int main(void) {
+
+    printf("hello, Marina\n")
     // int sz = 100;
 
     // int a[sz]
 
-    int age = 0;
+    // int age = 0;
 
-    printf("Put your age here: ");
-    scanf("%d", age);
-    printf("Your age is %i ", age);
-    getchar();
+    // printf("Put your age here: ");
+    // scanf("%d", age);
+    // printf("Your age is %i ", age);
+    // getchar();
     // //multiplyUntil(0, sz);
     // for (int i = 0; i < sz; ++i) {
     //      a++ = multiplyNumberBySelf(i);
